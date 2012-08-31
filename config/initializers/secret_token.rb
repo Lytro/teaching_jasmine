6006e3d18d9755af70c1b5d58267640d5625e289
Anuj Biyani <abiyani@lytro.com>
2012-08-31 15:09:14 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TeachingJasmine::Application.config.secret_token = 'b02c87abef0cdc96836bca500efa628cc6032bf00bef550e990af98c4fae50443af7d18c0d15c9b0ef56468ad55e35807596f73e87b468427f4d4b0a667939a7'
