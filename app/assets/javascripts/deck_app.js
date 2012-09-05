7c71e8f0806cab569ca56a6087c5df175a28cedf
Anuj Biyani <abiyani@lytro.com>
2012-09-04 18:58:36 -0700

    sample specs+code written

---
+++ app/assets/javascripts/deck_app.js
@@ -0,0 +1,54 @@
+function Deck() {
+  this.cards = [];
+
+  for (var i = 0; i < 13; i++) {
+    this.cards.push(new Card('heart', i));
+  }
+  for (i = 0; i < 13; i++) {
+    this.cards.push(new Card('diamond', i));
+  }
+  for (i = 0; i < 13; i++) {
+    this.cards.push(new Card('club', i));
+  }
+
+  for (i = 0; i < 13; i++) {
+    this.cards.push(new Card('spade', i));
+  }
+}
+
+function Card(suite, value) {
+  this.suite = suite;
+  this.value = value;
+}
+
+Deck.prototype.shuffle = function() {
+  for (var i = 0; i < 2; i++) {
+    for (var j = 0; j < this.cards.length; j++) {
+      var k = Math.floor(Math.random() * this.cards.length);
+      var temp = this.cards[j];
+      this.cards[j] = this.cards[k];
+      this.cards[k] = temp;
+    }
+  }
+};
+
+Deck.prototype.pickCard = function() {
+  return this.cards[Math.floor((Math.random()*52))];
+};
+
+Deck.prototype.throwCard = function() {
+  var deck = this;
+
+  $.ajax({
+    url: 'card/throw.json',
+    type: "GET",
+    dataType: "json",
+    contentType: "application/json; charset=utf-8",
+    success: function (data) {
+      deck.throwCardCallback(data);
+    }
+  });
+};
+
+Deck.prototype.throwCardCallback = function(data) {
+};
---
+++ spec/javascripts/deck_app_spec.js
@@ -0,0 +1,66 @@
+describe("Deck", function () {
+  var deck;
+
+  beforeEach(function () {
+    deck = new Deck();
+  });
+
+  it("builds a deck of 52 cards", function () {
+    expect(deck.cards.length).toEqual(52);
+  });
+
+  it("has unique cards", function () {
+    for (var i = 0; i < 52; i++) {
+      for (var j = 0; j < 52; j++) {
+        if (i != j) {
+          expect(deck.cards[i]).not.toEqual(deck.cards[j]);
+        }
+      }
+    }
+  });
+
+  describe("Card", function () {
+    it("has a suite and value", function () {
+      var card = new Card('heart', 3);
+
+      expect(card.suite).toEqual('heart');
+      expect(card.value).toEqual(3);
+    });
+  });
+
+  it("guesses your card", function () {
+    deck.shuffle();
+    spyOn(deck, 'pickCard').andReturn(deck.cards[0]);
+
+    var yourCard = deck.cards[0];
+    deck.shuffle();
+
+    expect(deck.pickCard()).toEqual(yourCard);
+  });
+
+  it("guesses your card", function () {
+    deck.shuffle();
+    spyOn(deck, 'cards').andReturn(deck.cards[0]);
+
+    var yourCard = deck.cards[0];
+    deck.shuffle();
+
+    expect(deck.cards[0]).toEqual(yourCard);
+  });
+
+  it("throws your card", function () {
+    var jsonResponse = {
+      suite: 'heart',
+      value: '2'
+    };
+
+    spyOn($, 'ajax').andCallFake(function (options) {
+      options.success(jsonResponse);
+    });
+    spyOn(deck, 'throwCardCallback');
+
+    deck.throwCard();
+    expect(deck.throwCardCallback).toHaveBeenCalled();
+    expect(deck.throwCardCallback).toHaveBeenCalledWith(jsonResponse);
+  });
+});
